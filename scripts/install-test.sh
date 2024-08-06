9131a29cfd925d76500b30ad6a37b379a427da9c
Timothy Drysdale <timothy.d.drysdale@gmail.com>
2024-08-06 13:41:41 +0100

    pyproject.toml (chore): add install_requires

--- scripts/install-test.sh
+++ scripts/install-test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -m pip install --upgrade --force-reinstall --index-url https://test.pypi.org/simple/ --no-deps practable
+python3 -m pip install --upgrade --force-reinstall --index-url https://test.pypi.org/simple/ practable
