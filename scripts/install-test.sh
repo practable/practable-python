75f82260df5e867d460d56981ca7235defcf3004
Timothy Drysdale <timothy.d.drysdale@gmail.com>
2024-08-06 13:50:45 +0100

    scripts (fix): make install-test get deps

--- scripts/install-test.sh
+++ scripts/install-test.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python3 -m pip install --upgrade --force-reinstall --index-url https://test.pypi.org/simple/ practable
+# extra-index-url gets the deps
+python -m pip install --upgrade --force-reinstall --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple practable
+
